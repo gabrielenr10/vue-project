c2782ebd46d3f5b4cfc9da7ce6b5abea0fa71f72
Gabriel Iglesias <gabrielenr10@gmail.com>
2024-02-05 08:45:45 +0100

    Progreso clase 1

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,39 @@
 <script setup>
-import { ref } from "vue";
+import { ref } from 'vue'
 
-const header = ref("Shopping List App");
+const titulo = ref('Lista de la compra')
+const productos = ref([])
+const productoNuevo = ref('')
+const esImportante = ref(false)
+
+const añadirALaLista = () => {
+  productos.value.push({
+    id: productos.value.length + 1,
+    nombre: productoNuevo.value
+  })
+  productoNuevo.value = ''
+}
 </script>
 
 <template>
-  <h1>{{ header }}</h1>
+  <h1>{{ titulo }}</h1>
+  <form class="add-item-form" @submit.prevent="añadirALaLista()">
+    <input
+      v-model="productoNuevo"
+      type="text"
+      placeholder="Que quieres comprar?"
+    />
+    <label>
+      <input v-model="esImportante" type="checkbox" />
+      Muy importante
+    </label>
+    <button class="btn btn-primary">Añadir producto</button>
+  </form>
+  <ul>
+    <li v-for="{ id, nombre } in productos" :key="id">
+      {{ nombre }} - {{ id }}
+    </li>
+  </ul>
+
+  <p v-if="!productos.length">La lista está vacia</p>
 </template>
